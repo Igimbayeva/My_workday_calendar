005b82c542c805e51ba16e1fb76efd527db347d2
Igimbayeva <igimbayevadinara@gmail.com>
2024-02-26 02:46:09 -0500

    Created a js code that generates the timeblocks, applies time classes based on current time, saves and loads events to and from the local storage. Additionally, corrected paths to the src files and updated the descriptions.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,43 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(document).ready(function(){
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+$(document).ready(function () {
+  
+  // Function to generate time blocks for 9AM to 5PM
+  function generateTimeBlocks() {
+    for (let i = 9; i <= 17; i++) {
+      const timeblock = $(`#hour-${i}`);
+      const currentHour = dayjs().hour();
+
+      // Add past, present, or future class based on the current time
+      if (i < currentHour) {
+        timeblock.removeClass("present future").addClass("past");
+      } else if (i === currentHour) {
+        timeblock.removeClass("past future").addClass("present");
+      } else {
+        timeblock.removeClass("past present").addClass("future");
+      }
+
+      // Load saved events from localStorage
+      const savedEvent = localStorage.getItem(`hour-${i}`);
+      if (savedEvent) {
+        timeblock.find(".description").val(savedEvent);
+      }
+    }
+  }
+
+  // Function to save events to localStorage
+  $(".saveBtn").on("click", function () {
+    var blockId = $(this).parent().attr("id");
+    var eventText = $(this).siblings(".description").val();
+
+    localStorage.setItem(blockId, eventText);
+  });
+
+  // Display current date in the header
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY, HH:MM A"));
+
+  // Generate time blocks
+  generateTimeBlocks();
+
+  // Update time block colors every minute
+  setInterval(generateTimeBlocks, 60000);
 });
